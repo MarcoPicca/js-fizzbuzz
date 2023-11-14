2bdd1e22c94bd3f7afbb7069edc85259927c4ae1
MarcoPicca <il96marco@gmail.com>
2023-11-14 15:31:45 +0100

    FizzBuzz funzionante

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
 let containerElement = document.querySelector('container');
 
 for (i = 1; i <= 100; i = i + 1) {
-
-    if (i % 3 === 0) {
-        let i = 'Fizz';
-        console.log(i);
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
     } else if (i % 5 === 0) {
-        let i = 'Buzz';
-        console.log(i);
-    } else if (i % 3 === 0 && i % 5 === 0) {
-        let i = 'FizzBuzz';
+        console.log('Buzz');
+    } else {
         console.log(i);
     }
-    console.log(i);
 }
